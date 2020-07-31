2e1a9e0a828fe33cd4cb38e16b4cd908701735c4
Roger  Acosta <rogeracosta359@gmail.com>
2020-07-30 21:00:53 -0500

    dogfactory example

--- JS_tutorials_apps/Basics_Projects/js/codechanllenges.js
+++ JS_tutorials_apps/Basics_Projects/js/codechanllenges.js
@@ -259,3 +259,38 @@ function findMyKeys(arr) {
 const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
 
 console.log(findMyKeys(randomStuff))
+
+/// dogFactory() exercise
+// Write your code here:
+function dogFactory(name,breed,weight){
+  return {
+    _name: name,
+    _breed: breed,
+    _weight:weight,
+    get name(){
+      return this._name
+    },
+    set name(nameIn){
+      this._name = nameIn
+    },
+    get breed(){
+      return this._breed
+    },
+    set breed(breedIn){
+      this._breed = breedIn
+    },
+    get weight(){
+      return this._weight
+    },
+    set weight(weightIn){
+      this._weight = weightIn
+    },
+    bark(){
+      return 'ruff! ruff!'
+    },
+    eatTooManyTreats(){
+      return this._weight++
+    }
+  }
+}
+
